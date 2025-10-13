9ee770143f9aff2376c782aab9d4429c1831b01b
Bintang Akbar Sutjipto <123040376+BintangAkbarSutjipto@users.noreply.github.com>
2025-10-13 10:40:55 +0700

    tes

---
+++ C++/nama.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	string nama = "Haein 💜";
+	int umur = 18;
+	float tinggi=172;
+	bool aktif = true;
+
+	cout << "Nama: " << nama << endl;
+	cout << "Umur: " << umur << endl;
+	cout << "Tinggi: " << tinggi << endl;
+	cout << "Status aktif: " << aktif << endl;
+
+	return 0;
+}
