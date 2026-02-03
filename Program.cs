4c1856997ceabefc26b8b01f22aca3e65b58c3c1
Bintang Akbar Sutjipto <123040376+BintangAkbarSutjipto@users.noreply.github.com>
2026-02-03 20:54:03 +0700

    new

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int nilai = 75;
+
+        if (nilai >= 70)
+        {
+            Console.WriteLine("Selamat, Anda LULUS.");
+        }
+        else
+        {
+            Console.WriteLine("Maaf, Anda TIDAK LULUS.");
+        }
+    }
+}
---
+++ Python/if.py
@@ -0,0 +1,6 @@
+nilai = 75
+
+if nilai >= 70:
+    print("Selamat, Anda LULUS")
+else:
+    print("Maaf, Anda TIDAK LULUS")
